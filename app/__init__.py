076b31dff67b6dac85306ee5f0d95e992d8340f4
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-01 21:30:46 +0300

    added  app configs

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='fd8ab5bd9d7f18715270fa3dd0005b645c7d7baae0967d0dbcd3d170caf70593'
-app.config['SQLALCHEMY_DATABASE-URI'] = 'sqlite:///abed.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///abnas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
